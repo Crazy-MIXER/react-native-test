318a130b8eec2b8928e63cf129e2f64f7c2856d7
Max <maskalyov.maks@gmail.com>
2022-11-06 19:20:23 +0300

    added intarface for UserItem, uploading&show data

--- App.tsx
+++ App.tsx
@@ -1,44 +1,89 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, StatusBar, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Image, StatusBar, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-];
+interface userProps {
+  email: string
+  name: {
+    first: string
+    last: string
+    title: string
+  }
+  picture: {
+    large: string
+    medium: string
+    thumbnail: string
+  }
+  phone: string
+  location: {
+    city: string
+    country: string
+  }
+}
 
-const Item =({item}:any, {onPress}:any)=>{
-  <TouchableOpacity
-    onPress={onPress}
-  >
+const UserItem = (props: userProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={() => { }}
+    >
+      <View style={styles.item}>
+        <View style={{ width: '60%' }}>
+          <Text style={{ fontSize: 20 }}> {props.name.title} {props.name.first} {props.name.last} </Text>
+          <Text style={{ fontSize: 17, color: 'silver' }}> {props.email} </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
-  </TouchableOpacity>
+function UserList() {
+  const [loading, setLoading] = useState(true)
+  const [users, setUsers] = useState([])
+  const [error, setError] = useState(false);
+
+  async function getUsers() {
+    try {
+      const usersData = await fetch('https://randomuser.me/api/?results=5')
+        .then((response) => response.json())
+      setUsers(usersData.results)
+    }
+    catch (err) {
+      setError(true)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  if (error)
+    return <Text style={styles.errorMsg}> Unable to connect to the server. </Text>
+
+  if (loading)
+    return <ActivityIndicator size='large' color='#00ff00' style={{ marginTop: 15 }} />
+
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      data={users}
+      renderItem={({ item }:any) => <UserItem {...item} />}
+    />
+  )
 }
 
 export default function App() {
   const heart = require('./components/icons/heart.png')
   const refresh = require('./components/icons/refresh.png')
 
-  const renderItem = ({item}:any) =>{
-
-
-  }
-
   return (
     <View style={styles.header}>
       <View style={styles.head}>
         <TouchableOpacity
           activeOpacity={0.6}
-          onPress={()=>{}}
+          onPress={() => { }}
         >
           <Image
             style={styles.icon}
@@ -48,7 +93,7 @@ export default function App() {
         <Text style={{ fontWeight: '400' }}>  Users list </Text>
         <TouchableOpacity
           activeOpacity={0.6}
-          onPress={()=>{}}
+          onPress={() => { }}
         >
           <Image
             style={styles.icon}
@@ -56,11 +101,7 @@ export default function App() {
           />
         </TouchableOpacity>
       </View>
-      <FlatList
-        style={{flex:1}}
-        data={DATA}
-        renderItem={renderItem}
-      />
+      <UserList />
     </View>
   );
 }
@@ -82,5 +123,18 @@ const styles = StyleSheet.create({
   header: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
-  }
+  },
+  item: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    backgroundColor: '#C5E1F1',
+    width: '100%'
+  },
+  errorMsg: {
+    color: '#ee8888',
+    fontSize: 20,
+    padding: 24,
+    textAlign: 'center'
+  },
 });
